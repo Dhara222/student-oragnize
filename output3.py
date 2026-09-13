d494012e4d6d450f3a8f3aadd17b2ab0ba3ae7d5
Dhara222 <dharaaa369@gmail.com>
2026-09-13 14:45:12 +0530

    Add files via upload

---
+++ output3.py
@@ -0,0 +1,113 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+================== RESTART: C:\Users\Admin\Desktop\trial12.py ==================
+Welcome to the student data Organizer!
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 1
+
+***add student***
+
+
+Enter student details: 
+student id: 103
+Name: dhara
+Age: 19
+Grade: A
+Date of Birth (YYYY-MM-DD): 2007-06-02
+Subjects (comma-separated): maths, sci, python 
+
+ Student added Successfully 
+
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 2
+
+***Display All Students***
+
+Student ID: 103 | Name: dhara | Age: 19 | Grade: A | Date: 2007-06-02 | Subjects: maths, sci, python
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 3
+
+***Update student information***
+
+Enter student ID: 103
+Enter your New Name: khushi
+Enter your New Age: 20
+Enter your New Grade: A+
+Enter your New Subjects: maths, hindi, OOP
+
+Student ID: 103
+Name: khushi
+Age: 20
+Grade: A+
+Date: 2007-06-02
+Subjects: ['maths, hindi, OOP']
+
+Your information updated successfully
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 4
+
+***Delete Student***
+
+Enter student ID: 103
+
+Student deleted successfully
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 5
+
+***Subjects Offered***
+
+maths, hindi, OOP
+maths, sci, python
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 6
+
+Thank you! Program exited.
