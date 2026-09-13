09a7ca30d846e0242b595ccd7cb0f03c6ab785f4
Dhara222 <dharaaa369@gmail.com>
2026-09-13 16:28:49 +0530

    Add files via upload

---
+++ 3output.py
@@ -0,0 +1,137 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============ RESTART: C:\Users\Admin\Desktop\py3project\project3.py ============
+Welcome to the student data Organizer!
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 1
+
+***add student***
+
+
+Enter student details: 
+student id: 103
+Name: dhara
+Age: 19
+Grade: A+
+Date of Birth (YYYY-MM-DD): 2007-06-02
+Subjects (comma-separated): Maths, sci, python, ai
+
+ Student added Successfully 
+
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 1
+
+***add student***
+
+
+Enter student details: 
+student id: 101
+Name: khushi
+Age: 21
+Grade: A
+Date of Birth (YYYY-MM-DD): 2006-02-22
+Subjects (comma-separated): maths, ai, ml, python
+
+ Student added Successfully 
+
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 2
+
+***Display All Students***
+
+Student ID: 103 | Name: dhara | Age: 19 | Grade: A+ | Date: 2007-06-02 | Subjects: Maths, sci, python, ai
+Student ID: 101 | Name: khushi | Age: 21 | Grade: A | Date: 2006-02-22 | Subjects: maths, ai, ml, python
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 3
+
+***Update student information***
+
+Enter student ID: 103
+Enter your New Name: vishal
+Enter your New Age: 24
+Enter your New Grade: A
+Enter your New Subjects: bio, sci, physics
+
+Student ID: 103
+Name: vishal
+Age: 24
+Grade: A
+Date: 2007-06-02
+Subjects: ['bio, sci, physics']
+
+Your information updated successfully
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 4
+
+***Delete Student***
+
+Enter student ID: 101
+
+Student deleted successfully
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 5
+
+--- Subjects Offered ---
+Unique Subjects Offered:
+- bio, sci, physics
+
+Select an option: 
+1. Add Student
+2. Display All Students
+3. Update Student Information
+4. Delete Student
+5. Display Subjects Offered
+6. Exit
+
+ Enter your choice: 6
+
+Thank you! Program exited.
---
+++ project3.py
@@ -0,0 +1,141 @@
+students = []
+student_data = {}
+subject_set= set()
+
+print("Welcome to the student data Organizer!")
+
+while True:
+    print("\nSelect an option: ")
+    print("1. Add Student")
+    print("2. Display All Students")
+    print("3. Update Student Information")
+    print("4. Delete Student")
+    print("5. Display Subjects Offered")
+    print("6. Exit")
+
+    choice = int(input("\n Enter your choice: "))
+
+    if choice==1:
+        print("\n***add student***\n")
+        print("\nEnter student details: ")
+        student = int(input("student id: "))
+
+        if student in student_data:
+            print("student already exits")
+        else:
+            Name=input("Name: ")
+            Age=int(input("Age: "))
+            Grade=input("Grade: ")
+            Date=input("Date of Birth (YYYY-MM-DD): ")
+            Subjects=input("Subjects (comma-separated): ")
+
+            Subjects= Subjects.split(" , ")
+            Subjects=[Subject.strip() for Subject in Subjects]
+            subject_set.update(Subjects)
+            
+
+            student_info = (student,Date)
+            
+            student_data={
+            student :{
+                    "info": student_info,
+                     "name": Name,
+                     "Age": Age,
+                     "Grade": Grade,
+                     "Date" : Date,
+                     "Subjects": Subjects}}
+
+            students.append(student_data)
+
+            print("\n Student added Successfully \n")
+
+            
+    elif choice == 2:
+         print("\n***Display All Students***\n")
+         for student in students:
+             for student_id, details in student.items():
+                print(f"Student ID: {student_id} | Name: {details['name']} | Age: {details['Age']} | Grade: {details['Grade']} | Date: {details['Date']} | Subjects: {','.join(details['Subjects'])}")
+
+
+    elif choice == 3:
+         print("\n***Update student information***\n")
+         student_id_input = int(input("Enter student ID: "))
+
+         for student in students:
+             for student_id, details in student.items():
+                 if student_id == student_id_input:
+                     new_name = input("Enter your New Name: ")
+                     new_age = int(input("Enter your New Age: "))
+                     new_grade = input("Enter your New Grade: ")
+                     new_subjects= [Subject.strip()for Subject in input("Enter your New Subjects: ").split(" , ")]
+
+                     details["name"] = new_name
+                     details["Age"] = new_age
+                     details["Grade"] = new_grade
+                     details["Subjects"] = new_subjects
+                     subject_set.update(new_subjects)
+                    
+                     print(f"\nStudent ID: {student_id}")
+                     print(f"Name: {details['name']}")
+                     print(f"Age: {details['Age']}")
+                     print(f"Grade: {details['Grade']}")
+                     print(f"Date: {details['Date']}")
+                     print(f"Subjects: {details['Subjects']}")
+                     print("\nYour information updated successfully")
+         
+    elif choice == 4:
+        
+        print("\n***Delete Student***\n")
+
+        student_id_input = int(input("Enter student ID: "))
+
+        for i in range(len(students)):
+            for student_id in students[i]:
+                if student_id == student_id_input:
+                    del students[i]
+                    print("\nStudent deleted successfully")
+                    break
+                
+    elif choice == 5:
+        print("\n***Subjects Offered ***\n")
+
+        all_subjects = set()
+
+        for student in students:
+            for student_id, details in student.items():
+                all_subjects.update(details["Subjects"])
+
+        if not all_subjects:
+
+            print("No subjects found!")
+
+        else:
+
+            print("Unique Subjects Offered:")
+
+            for subject in sorted(all_subjects):
+
+                print("-", subject)
+
+    elif choice == 6:
+        print("\nThank you! Program exited.")
+        break
+              
+            
+
+            
+            
+                         
+           
+            
+            
+
+
+
+
+
+
+
+
+
+
